a76e7cb9c56bdf162661bf537c107ffc7eda07ef
ben avrahami <dontcallme@illcall.you>
2020-10-08 02:11:59 +0300

    lint

--- expressive/__init__.py
+++ expressive/__init__.py
@@ -1,3 +1,9 @@
+# flake8: noqa F403, F401
 from expressive.single import *
 from expressive.delayed import *
 from expressive._version import __version__
+
+from expressive.single import __all__ as single_all
+from expressive.delayed import __all__ as delayed_all
+
+__all__ = ['__version__', *single_all, *delayed_all]
